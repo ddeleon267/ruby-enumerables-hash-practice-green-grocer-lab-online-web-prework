9f5f8eb4cb28560e9a249a5538537df681b6ff4d
github username <github email address>
2019-07-01 18:48:09 -0400

    Get consolidate_cart passing.

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,31 @@
+require "pry"
+
 def consolidate_cart(cart)
-  # code here
+  consolidated_cart = {}
+
+  cart.each do |item|
+    item_name = item.keys.first
+    if consolidated_cart.keys.include?(item_name)
+      consolidated_cart[item_name][:count] += 1
+    else
+       consolidated_cart[item_name] = {
+           price: item[item_name][:price],
+           clearance: item[item_name][:clearance],
+           count: 1
+       }
+    end
+  end
+  consolidated_cart
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+
 end
 
 def apply_clearance(cart)
-  # code here
+
 end
 
 def checkout(cart, coupons)
-  # code here
+
 end
